30ce07b3e1de038b30acd2e3a40d40b79c999867
winner <winnerson.purba@mail.ugm.ac.id>
2022-06-12 20:40:25 +0700

    try challenge

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs')
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (callback) => {
+  const wordData = []
+  console.log(file1, file2, file3);
+  fs.readFile(file1, (err, data) => {
+    if (err) return callback(new Error(err), null)
+    let word = (JSON.parse(data)).message
+
+    if (word.split(" ").length < 2) {
+      return callback(`Ada kata yang kurang dari 2`, null)
+    }
+    wordData.push(word.split(" ")[1])
+    fs.readFile(file2, (err, data) => {
+      if (err) return callback(new Error(err), null)
+      let word = JSON.parse(data)[0].message
+
+      if (word.split(" ").length < 2) {
+        return callback(`Ada kata yang kurang dari 2`, null)
+      }
+      wordData.push(word.split(" ")[1])
+      fs.readFile(file3, (err, data) => {
+        if (err) return callback(new Error(err), null)
+        let word = JSON.parse(data)[0].data.message
+
+        if (word.split(" ").length < 2) {
+          return callback(`Ada kata yang kurang dari 2`, null)
+        }
+        wordData.push(word.split(" ")[1])
+        return callback(null, wordData)
+      })
+    })
+  })
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
